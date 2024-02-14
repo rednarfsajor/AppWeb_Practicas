367fc5c3472d334c82bbdcfca3e980c28d0f0327
rednarfsajor <frojas20190@ufide.ac.cr>
2024-02-14 06:02:18 -0600

    Practica 2
    Finalización de Practica 2

---
+++ Practica_2_Frander_Rojas/src/main/java/com/Practica_2_Frander_Rojas/Practica_2_Frander_Rojas/Practica2FranderRojasApplication.java
@@ -0,0 +1,13 @@
+package com.Practica_2_Frander_Rojas.Practica_2_Frander_Rojas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica2FranderRojasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica2FranderRojasApplication.class, args);
+	}
+
+}
---
+++ Practica_2_Frander_Rojas/src/test/java/com/Practica_2_Frander_Rojas/Practica_2_Frander_Rojas/Practica2FranderRojasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Practica_2_Frander_Rojas.Practica_2_Frander_Rojas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica2FranderRojasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
